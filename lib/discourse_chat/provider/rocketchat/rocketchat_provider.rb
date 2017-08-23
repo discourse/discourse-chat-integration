5ea6553236ed18da4323217aa5d8effdf6d31084
David Taylor <david@taylorhq.com>
2017-08-23 16:43:27 +0100

    Add support for Rocket.Chat

---
+++ lib/discourse_chat/provider/rocketchat/rocketchat_provider.rb
@@ -0,0 +1,74 @@
+module DiscourseChat::Provider::RocketchatProvider
+  PROVIDER_NAME = "rocketchat".freeze
+
+  PROVIDER_ENABLED_SETTING = :chat_integration_rocketchat_enabled
+
+  CHANNEL_PARAMETERS = [
+                        { key: "identifier", regex: '^[@#]\S*$', unique: true }
+                       ]
+
+  def self.rocketchat_message(post, channel)
+    display_name = "@#{post.user.username}"
+    full_name = post.user.name || ""
+
+    if !(full_name.strip.empty?) && (full_name.strip.gsub(' ', '_').casecmp(post.user.username) != 0) && (full_name.strip.gsub(' ', '').casecmp(post.user.username) != 0)
+      display_name = "#{full_name} @#{post.user.username}"
+    end
+
+    topic = post.topic
+
+    category = ''
+    if topic.category
+      category = (topic.category.parent_category) ? "[#{topic.category.parent_category.name}/#{topic.category.name}]" : "[#{topic.category.name}]"
+    end
+
+    message = {
+      channel: channel,
+      attachments: []
+    }
+
+    summary = {
+      fallback: "#{topic.title} - #{display_name}",
+      author_name: display_name,
+      author_icon: post.user.small_avatar_url,
+      color: topic.category ? "##{topic.category.color}" : nil,
+      text: post.excerpt(SiteSetting.chat_integration_rocketchat_excerpt_length, text_entities: true, strip_links: true, remap_emoji: true),
+      mrkdwn_in: ["text"],
+      title: "#{topic.title} #{(category == '[uncategorized]') ? '' : category} #{topic.tags.present? ? topic.tags.map(&:name).join(', ') : ''}",
+      title_link: post.full_url,
+      thumb_url: post.full_url
+    }
+
+    message[:attachments].push(summary)
+
+    return message
+  end
+
+  def self.send_via_webhook(message)
+    uri = URI(SiteSetting.chat_integration_rocketchat_webhook_url)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+    req.body = message.to_json
+    response = http.request(req)
+
+    unless response.kind_of? Net::HTTPSuccess
+      if response.body.include?('invalid-channel')
+        error_key = 'chat_integration.provider.rocketchat.errors.invalid_channel'
+      else
+        error_key = nil
+      end
+      raise ::DiscourseChat::ProviderError.new info: { error_key: error_key, request: req.body, response_code: response.code, response_body: response.body }
+    end
+
+  end
+
+  def self.trigger_notification(post, channel)
+    channel_id = channel.data['identifier']
+    message = rocketchat_message(post, channel_id)
+
+    self.send_via_webhook(message)
+  end
+end
---
+++ spec/lib/discourse_chat/provider/rocketchat/rocketchat_provider_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe DiscourseChat::Provider::RocketchatProvider do
+  let(:post) { Fabricate(:post) }
+
+  describe '.trigger_notifications' do
+    before do
+      SiteSetting.chat_integration_rocketchat_enabled = true
+      SiteSetting.chat_integration_rocketchat_webhook_url = "https://example.com/abcd"
+    end
+
+    let(:chan1) { DiscourseChat::Channel.create!(provider: 'rocketchat', data: { identifier: "#general" }) }
+
+    it 'sends a webhook request' do
+      stub1 = stub_request(:post, 'https://example.com/abcd').to_return(body: "{\"success\":true}")
+      described_class.trigger_notification(post, chan1)
+      expect(stub1).to have_been_requested.once
+    end
+
+    it 'handles errors correctly' do
+      stub1 = stub_request(:post, 'https://example.com/abcd').to_return(status: 400, body: "{}")
+      expect(stub1).to have_been_requested.times(0)
+      expect { described_class.trigger_notification(post, chan1) }.to raise_exception(::DiscourseChat::ProviderError)
+      expect(stub1).to have_been_requested.once
+    end
+
+  end
+
+end
