da9ee8dcb5a07ab592bc4a5b07da47454674660f
David Taylor <david@taylorhq.com>
2017-11-20 12:49:36 +0000

    Fix mattermost icon selection

--- spec/lib/discourse_chat/provider/mattermost/mattermost_provider_spec.rb
+++ spec/lib/discourse_chat/provider/mattermost/mattermost_provider_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe DiscourseChat::Provider::MattermostProvider do
     before do
       SiteSetting.chat_integration_mattermost_enabled = true
       SiteSetting.chat_integration_mattermost_webhook_url = "https://mattermost.blah/hook/abcd"
+      SiteSetting.logo_small_url = "https://some_small_logo"
     end
 
     let(:chan1) { DiscourseChat::Channel.create!(provider: 'mattermost', data: { identifier: "#awesomechannel" }) }
@@ -15,6 +16,21 @@ RSpec.describe DiscourseChat::Provider::MattermostProvider do
       stub1 = stub_request(:post, 'https://mattermost.blah/hook/abcd').to_return(status: 200)
       described_class.trigger_notification(post, chan1)
       expect(stub1).to have_been_requested.once
+    end
+
+    describe 'when mattermost icon is not configured' do
+      it 'defaults to the right icon' do
+        message = described_class.mattermost_message(post, chan1)
+        expect(message[:icon_url]).to eq(SiteSetting.logo_small_url)
+      end
+    end
+
+    describe 'when mattermost icon has been configured' do
+      it 'should use the right icon' do
+        SiteSetting.chat_integration_mattermost_icon_url = "https://specific_logo"
+        message = described_class.mattermost_message(post, chan1)
+        expect(message[:icon_url]).to eq(SiteSetting.chat_integration_mattermost_icon_url)
+      end
     end
 
     it 'handles errors correctly' do
