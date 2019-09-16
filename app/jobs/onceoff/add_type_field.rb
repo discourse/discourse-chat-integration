154361e22c77b75b48ba911862eb29fdd4c466d8
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2019-09-17 03:38:30 +1000

    FIX: Use top-level namespace for base classes (#33)

--- app/jobs/onceoff/add_type_field.rb
+++ app/jobs/onceoff/add_type_field.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Jobs
-  class DiscourseChatAddTypeField < Jobs::Onceoff
+  class DiscourseChatAddTypeField < ::Jobs::Onceoff
     def execute_onceoff(args)
       DiscourseChat::Rule.find_each do |rule|
         rule.save(validate: false)
