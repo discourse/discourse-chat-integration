9f9816b0c5989578e1c259a569e11978b8736c18
tosin <tosin.sonuyi@gmail.com>
2020-05-08 04:36:58 -0400

    rework to use bots groupme api intstead of developer, no access tokens reqd
    also should catch 404 response codes

--- lib/discourse_chat/provider/groupme/groupme_provider.rb
+++ lib/discourse_chat/provider/groupme/groupme_provider.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
-require 'securerandom'
 module DiscourseChat::Provider::GroupmeProvider
     PROVIDER_NAME = "groupme".freeze
   
     PROVIDER_ENABLED_SETTING = :chat_integration_groupme_enabled
   
-    CHANNEL_PARAMETERS = [
-                          { key: "identifier", regex: '^[@#]\S*$', unique: true },
-                          { key: "webhook_url", regex: '^https://api.groupme.com/v3/groups/*/messages'}
-                         ]
+    CHANNEL_PARAMETERS = []
   
-    def self.generate_groupme_message(post, channel)
+    def self.generate_groupme_message(post)#, channel)
       display_name = "@#{post.user.username}"
       full_name = post.user.name || ""
   
@@ -27,45 +23,38 @@ module DiscourseChat::Provider::GroupmeProvider
         category = (topic.category.parent_category) ? "[#{topic.category.parent_category.name}/#{topic.category.name}]" : "[#{topic.category.name}]"
       end
 
+      pre_post_text = "#{display_name}: #{topic.title}(#{post.full_url}) #{category} #{topic.tags.present? ? topic.tags.map(&:name).join(', ') : ''}"
       data = {
-          message: {
-              source_guid: SecureRandom.uuid,
-              text: post.excerpt(SiteSetting.chat_integration_groupme_excerpt_length, text_entities: true, strip_links: true, remap_emoji: true)
-          }
+        text: "#{pre_post_text} - #{post.excerpt(SiteSetting.chat_integration_groupme_excerpt_length, text_entities: true, strip_links: true, remap_emoji: true)}"
       }
 
     end
   
     def self.send_via_webhook(message)
-      # uri = URI(SiteSetting.chat_integration_groupme_webhook_url)
-      # for loop through all the group IDs
-      group_ids = SiteSetting.chat_integration_groupme_groupids.split(/\s*,\s*/)
-      group_ids.each { |group_id|
-        uri = URI("https://api.groupme.com/v3/groups/#{group_id}/messages?token=#{SiteSetting.chat_integration_groupme_access_token}")
+      # loop through all the bot IDs
+      bot_ids = SiteSetting.chat_integration_groupme_bot_ids.split(/\s*,\s*/)
+      bot_ids.each { |bot_id|
+        uri = URI("https://api.groupme.com/v3/bots/post")
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = (uri.scheme == 'https')
         req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+        message[:bot_id] = bot_id
         req.body = message.to_json
         response = http.request(req)
+        puts response
         unless response.kind_of? Net::HTTPSuccess
-            if response.body.include?('invalid-channel')
-              error_key = 'chat_integration.provider.groupme.errors.invalid_channel'
+            if response.body.response_code.include?('404')
+              error_key = 'chat_integration.provider.groupme.errors.not_found'
             else
               error_key = nil
             end
             raise ::DiscourseChat::ProviderError.new info: { error_key: error_key, request: req.body, response_code: response.code, response_body: response.body }
         end
-
       }
     end
   
-    # ideally i would get it to post multiple messages in the
-    # case that user wants multiple groupmes, only works if token 
-    # can be re-used
     def self.trigger_notification(post, channel)
-    #   channel_id = channel.data['identifier']
-      data_package = generate_groupme_message(post)#, channel_id)
-  
+      data_package = generate_groupme_message(post)
       self.send_via_webhook(data_package)
     end
   end
