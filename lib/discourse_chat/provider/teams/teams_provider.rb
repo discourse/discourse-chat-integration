fa5ef22262afe2740e0e7a9df9ad3fab5d8eddfe
Ahmed Gagan <ahmedgagan6@gmail.com>
2020-07-29 21:50:02 +0530

    FEATURE: Support for Microsoft Teams webhooks (#42)

---
+++ lib/discourse_chat/provider/teams/teams_provider.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+module DiscourseChat::Provider::TeamsProvider
+  PROVIDER_NAME = "teams".freeze
+  PROVIDER_ENABLED_SETTING = :chat_integration_teams_enabled
+  CHANNEL_PARAMETERS = [
+    { key: "name", regex: '^\S+$', unique: true },
+    { key: "webhook_url", regex: '^https:\/\/outlook\.office\.com\/webhook\/[A-Za-z0-9\-@\/]+\S+$', unique: true, hidden: true }
+  ]
+
+  def self.trigger_notification(post, channel, rule)
+    message = get_message(post)
+    uri = URI(channel.data['webhook_url'])
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+    req.body = message.to_json
+    response = http.request(req)
+
+    unless response.kind_of? Net::HTTPSuccess
+      if response.body.include?('Invalid webhook URL')
+        error_key = 'chat_integration.provider.teams.errors.invalid_channel'
+      else
+        error_key = nil
+      end
+      raise ::DiscourseChat::ProviderError.new info: { error_key: error_key, request: req.body, response_code: response.code, response_body: response.body }
+    end
+
+  end
+
+  def self.get_message(post)
+    display_name = "@#{post.user.username}"
+    full_name = post.user.name || ""
+
+    topic = post.topic
+
+    category = ''
+    if topic.category&.uncategorized?
+      category = "[#{I18n.t('uncategorized_category_name')}]"
+    elsif topic.category
+      category = (topic.category.parent_category) ? "[#{topic.category.parent_category.name}/#{topic.category.name}]" : "[#{topic.category.name}]"
+    end
+
+    message = {
+      "@type": "MessageCard",
+      "summary": topic.title,
+      "sections": [{
+        "activityTitle": "[#{topic.title} #{category} #{topic.tags.present? ? topic.tags.map(&:name).join(', ') : ''}](#{post.full_url})",
+        "activitySubtitle": post.excerpt(SiteSetting.chat_integration_teams_excerpt_length, text_entities: true, strip_links: true, remap_emoji: true),
+        "activityImage": post.user.small_avatar_url,
+        "facts": [{
+          "name": full_name,
+          "value": display_name
+        }],
+        "markdown": true
+      }],
+    }
+
+    message
+  end
+
+end
---
+++ spec/lib/discourse_chat/provider/teams/teams_provider_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DiscourseChat::Provider::TeamsProvider do
+  let(:post) { Fabricate(:post) }
+
+  describe '.trigger_notifications' do
+    before do
+      SiteSetting.chat_integration_teams_enabled = true
+    end
+
+    let(:chan1) { DiscourseChat::Channel.create!(provider: 'teams', data: { name: 'discourse', webhook_url: 'https://outlook.office.com/webhook/677980e4-e03b-4a5e-ad29-dc1ee0c32a80@9e9b5238-5ab2-496a-8e6a-e9cf05c7eb5c/IncomingWebhook/e7a1006ded44478992769d0c4f391e34/e028ca8a-e9c8-4c6c-a4d8-578f881a3cff' }) }
+
+    it 'sends a webhook request' do
+      stub1 = stub_request(:post, chan1.data['webhook_url']).to_return(body: "1")
+      described_class.trigger_notification(post, chan1, nil)
+      expect(stub1).to have_been_requested.once
+    end
+
+    it 'handles errors correctly' do
+      stub1 = stub_request(:post, chan1.data['webhook_url']).to_return(status: 400, body: "{}")
+      expect(stub1).to have_been_requested.times(0)
+      expect { described_class.trigger_notification(post, chan1, nil) }.to raise_exception(::DiscourseChat::ProviderError)
+      expect(stub1).to have_been_requested.once
+    end
+
+  end
+
+end
