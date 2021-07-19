c1951001d83b30bc9262bff0a034b74ed006eed2
msasso <matteo.sasso@gmail.com>
2021-07-19 10:06:23 +0200

    Update telegram_command_controller.rb

--- lib/discourse_chat_integration/provider/telegram/telegram_command_controller.rb
+++ lib/discourse_chat_integration/provider/telegram/telegram_command_controller.rb
@@ -62,18 +62,10 @@ module DiscourseChatIntegration::Provider::TelegramProvider
 
       channel = DiscourseChatIntegration::Channel.with_provider(provider).with_data_value('chat_id', chat_id).first
 
-      text_key = "unknown_chat" if channel.nil?
+      return if channel.nil?
       # If slash commands disabled, send a generic message
-      text_key = "known_chat" if !SiteSetting.chat_integration_telegram_enable_slash_commands
-      text_key = "help" if message['text'].blank?
-
-      if text_key.present?
-        return  I18n.t(
-          "chat_integration.provider.telegram.#{text_key}",
-          site_title: CGI::escapeHTML(SiteSetting.title),
-          chat_id: chat_id,
-        )
-      end
+      return if !SiteSetting.chat_integration_telegram_enable_slash_commands
+      return if message['text'].blank?
 
       tokens = message['text'].split(" ")
 
