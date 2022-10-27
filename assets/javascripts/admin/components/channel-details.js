7b8a1a396024d1eef2d65ad368aefc2f3b0694b7
Jan Cernik <66427541+jancernik@users.noreply.github.com>
2022-10-27 10:16:00 -0300

    DEV: Replace bootbox confirm dialog (#142)

--- assets/javascripts/admin/components/channel-details.js
+++ assets/javascripts/admin/components/channel-details.js
@@ -1,26 +1,23 @@
 import Component from "@ember/component";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import I18n from "I18n";
-import bootbox from "bootbox";
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
+  dialog: service(),
   classNames: ["channel-details"],
 
   actions: {
     deleteChannel(channel) {
-      bootbox.confirm(
-        I18n.t("chat_integration.channel_delete_confirm"),
-        I18n.t("no_value"),
-        I18n.t("yes_value"),
-        (result) => {
-          if (result) {
-            channel
-              .destroyRecord()
-              .then(() => this.refresh())
-              .catch(popupAjaxError);
-          }
-        }
-      );
+      this.dialog.deleteConfirm({
+        message: I18n.t("chat_integration.channel_delete_confirm"),
+        didConfirm: () => {
+          return channel
+            .destroyRecord()
+            .then(() => this.refresh())
+            .catch(popupAjaxError);
+        },
+      });
     },
 
     editRule(rule) {
