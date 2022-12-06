41cb6c4bd1cc195eee338c5b6dfc24b303692179
Keegan George <kgeorge13@gmail.com>
2022-12-06 09:59:06 -0800

    Update chat-integration-test component
    - Use angle bracket syntax
    - Native JS classes
    - Octane syntax/logic
    - Fix small CSS regression

--- assets/javascripts/admin/controllers/modals/admin-plugins-chat-integration-test.js
+++ assets/javascripts/admin/controllers/modals/admin-plugins-chat-integration-test.js
@@ -1,47 +1,39 @@
 import Controller from "@ember/controller";
+import { not } from "@ember/object/computed";
 import I18n from "I18n";
 import ModalFunctionality from "discourse/mixins/modal-functionality";
 import { ajax } from "discourse/lib/ajax";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import computed, { on } from "discourse-common/utils/decorators";
 import { schedule } from "@ember/runloop";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 
-export default Controller.extend(ModalFunctionality, {
-  @on("init")
-  setupKeydown() {
-    schedule("afterRender", () => {
-      $("#chat_integration_test_modal").keydown((e) => {
-        if (e.keyCode === 13) {
-          this.send("send");
-        }
-      });
-    });
-  },
+export default class AdminPluginsChatIntegrationTest extends Controller {
+  @not("model.topic_id") sendDisabled;
+  @tracked loading = false;
 
-  @computed("model.topic_id")
-  sendDisabled(topicId) {
-    return !topicId;
-  },
+  @action
+  send() {
+    if (this.sendDisabled) {
+      return;
+    }
+    this.loading = true;
 
-  actions: {
-    send() {
-      if (this.get("sendDisabled")) {
-        return;
-      }
-      this.set("loading", true);
-
-      ajax("/admin/plugins/chat-integration/test", {
-        data: {
-          channel_id: this.get("model.channel.id"),
-          topic_id: this.get("model.topic_id"),
-        },
-        type: "POST",
+    ajax("/admin/plugins/chat-integration/test", {
+      data: {
+        channel_id: this.model.channel.id,
+        topic_id: this.model.topic_id,
+      },
+      type: "POST",
+    })
+      .then(() => {
+        this.loading = false;
+        this.appEvents.trigger("modal-body:flash", { 
+          I18n.t("chat_integration.test_modal.success"), 
+          "success" 
+        });
       })
-        .then(() => {
-          this.set("loading", false);
-          this.flash(I18n.t("chat_integration.test_modal.success"), "success");
-        })
-        .catch(popupAjaxError);
-    },
-  },
-});
+      .catch(popupAjaxError);
+  }
+}
