bd9bf836ad1a4367b7c2247d8ad2a46e16897c6b
David Taylor <david@taylorhq.com>
2023-11-27 12:45:21 +0000

    DEV: Resolve router deprecation and only load assets for admin (#180)

--- admin/assets/javascripts/admin/routes/admin-plugins-chat-integration-index.js
+++ admin/assets/javascripts/admin/routes/admin-plugins-chat-integration-index.js
@@ -1,9 +1,12 @@
 import DiscourseRoute from "discourse/routes/discourse";
+import { inject as service } from "@ember/service";
 
 export default class AdminPluginsChatIntegrationIndex extends DiscourseRoute {
+  @service router;
+
   afterModel(model) {
     if (model.totalRows > 0) {
-      this.transitionTo(
+      this.router.transitionTo(
         "adminPlugins.chat-integration.provider",
         model.get("firstObject").name
       );
--- admin/assets/javascripts/admin/routes/admin-plugins-chat-integration.js
+++ admin/assets/javascripts/admin/routes/admin-plugins-chat-integration.js
@@ -1,14 +1,17 @@
 import DiscourseRoute from "discourse/routes/discourse";
 import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default class AdminPluginsChatIntegration extends DiscourseRoute {
+  @service router;
+
   model() {
     return this.store.findAll("provider");
   }
 
   @action
   showSettings() {
-    this.transitionTo("adminSiteSettingsCategory", "plugins", {
+    this.router.transitionTo("adminSiteSettingsCategory", "plugins", {
       queryParams: { filter: "chat_integration" },
     });
   }
