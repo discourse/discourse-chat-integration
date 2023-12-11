ebfd55d9a0d11bc21278f2a4561e3f92a865a132
Jarek Radosz <jradosz@gmail.com>
2023-12-11 13:10:56 +0100

    omg it wasn't used

--- admin/assets/javascripts/admin/components/modal/edit-rule.js
+++ admin/assets/javascripts/admin/components/modal/edit-rule.js
@@ -1,13 +1,10 @@
 import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 
 export default class EditRule extends Component {
   @service siteSettings;
-
-  @tracked saveDisabled = false;
 
   get showCategory() {
     return this.args.model.rule.type === "normal";
