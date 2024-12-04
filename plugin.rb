7969040a5599fbe430cdb14e201395d4bf2f2754
Kelvin Tan <kelv@discourse.org>
2024-12-04 09:09:08 +0800

    DEV: Update deprecated Font Awesome icon names

--- plugin.rb
+++ plugin.rb
@@ -12,7 +12,7 @@ enabled_site_setting :chat_integration_enabled
 register_asset "stylesheets/chat-integration.scss"
 
 register_svg_icon "rocket" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-arrow-circle-o-right" if respond_to?(:register_svg_icon)
+register_svg_icon "arrow-circle-o-right" if respond_to?(:register_svg_icon)
 
 # Site setting validators must be loaded before initialize
 require_relative "lib/discourse_chat_integration/provider/slack/slack_enabled_setting_validator"
